8020e0e8a87951fb78739a669950a961fcc11cd4
Myroslav Raks <raksmyroslav@gmail.com>
2024-06-09 18:10:56 +0200

    task 5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,10 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color");
+button.addEventListener("click", function(event) {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  document.querySelector(".color").textContent = newColor;
+});
