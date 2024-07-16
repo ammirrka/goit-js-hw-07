5a673afd11bb42ec3bc62a0069e93176ad340162
Myroslav Raks <raksmyroslav@gmail.com>
2024-07-16 19:07:40 +0200

    task 6 first try

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,25 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const container = document.querySelector('#boxes');
+function createBoxes(amount) {
+  let size = 30;
+  for (let i = 0; i <= amount; i++) {
+    const div = document.createElement('div');
+    container.insertAdjacentElement('beforeend', div);
+    div.style.width += `${size}px`;
+    div.style.height += `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    size += 10;
+  }
+}
+
+const input = document.querySelector('input');
+const createBtn = document.querySelector('[data-create]');
+
+createBtn.addEventListener('click', () => {
+  input.value <= 100 && input.value >= 1
+    ? createBoxes(input.value)
+    : alert('Please enter a valid number');
+});
